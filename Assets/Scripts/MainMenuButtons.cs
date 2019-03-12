5561fdaa6aff15bfda0198dc5f3b10b0b7bfe8d1
Zachary <zach@zachdavids.com>
2019-03-11 20:41:05 -0400

    Added Basic MainMenu Scene & Empty Cellar Scene
    Imported dungeon assets

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
